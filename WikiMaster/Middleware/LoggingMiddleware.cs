f76b398db548f89fc5d7941d033c82d3bd9ec489
d-starki <ircurlicus123@gmail.com>
2021-05-29 03:07:53 +0200

    Added logging requests

---
+++ WikiMaster/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiMaster.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public LoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<LoggingMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments("/api"))
+            {
+                _logger.LogInformation($"Http Request Information:{Environment.NewLine}" +
+                                       $"Schema:{context.Request.Scheme} " +
+                                       $"Host: {context.Request.Host} " +
+                                       $"Path: {context.Request.Path} ");
+            }
+
+            await _next(context);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class LoggerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLoggerMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoggingMiddleware>();
+        }
+    }
+}
